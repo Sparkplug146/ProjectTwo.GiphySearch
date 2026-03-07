bac257e1b15c2feaf082f489ecf583c544f4f6df
Keoni Inniss <ika84465@gmail.com>
2026-03-06 21:49:26 -0500

    Updated the js file to display gifs

--- main.js
+++ main.js
@@ -1,24 +1,41 @@
 const form = document.getElementById("search-form");
 const input = document.getElementById("search-input");
+const results = document.getElementById("results");
 console.log(input);
 
-form.addEventListener("submit", function(){
+form.addEventListener("submit", function(event){
     event.preventDefault();
 
     const searchTerm = input.value;
     console.log(searchTerm);
+
+    searchGifs(searchTerm);
 })
 
 const apiKey = "9rkKRrjFQIofMVpMWFXUvEjf0p3BAIEx";
 
 async function searchGifs(searchTerm) {
 
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=$9rkKRrjFQIofMVpMWFXUvEjf0p3BAIEx&q=${searchTerm}&limit=12`;
+   const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchTerm}&limit=12`;
 
     const response = await fetch(url);
 
     const data = await response.json();
 
     console.log(data);
+
+    results.innerHTML = "";
+
+data.data.forEach(function(gif) {
+
+    const gifUrl = gif.images.fixed_height.url;
+
+    const img = document.createElement("img");
+
+    img.src = gifUrl;
+
+    results.appendChild(img);
+
+});
 
 }
